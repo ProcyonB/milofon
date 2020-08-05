f6d24f36c20c8da472fb62783da5109ef0981786
ProcyonB <pelagea.nikolaevna@gmail.com>
2020-08-06 02:56:33 +0300

    spikmi+popups

--- milofon/js/script.js
+++ milofon/js/script.js
@@ -1,9 +1,44 @@
 $(document).ready(function () {
+  // модальные окна
+  $(".consultation").on("click", function (e) {
+    e.preventDefault();
+    $(".consultation_popup").fadeIn();
+  });
+  $(".close").click(function (e) {
+    e.preventDefault();
+    $(".consultation_popup").fadeOut();
+  });
+
+  $(".upgrade").on("click", function (e) {
+    e.preventDefault();
+    $(".upgrade_popup").fadeIn();
+  });
+  $(".close").click(function (e) {
+    e.preventDefault();
+    $(".upgrade_popup").fadeOut();
+  });
+
+  $(".rent").on("click", function (e) {
+    e.preventDefault();
+    $(".rent_popup").fadeIn();
+  });
+  $(".close").click(function (e) {
+    e.preventDefault();
+    $(".rent_popup").fadeOut();
+  });
+
+  $(".activities").on("click", function (e) {
+    e.preventDefault();
+    $(".activities_popup").fadeIn();
+  });
+  $(".close").click(function (e) {
+    e.preventDefault();
+    $(".activities_popup").fadeOut();
+  });
+
+  // слайдер
   $(".slider").slick({
     dots: true,
-    // infinite: true,
-    // speed: 500,
-    // fade: true,
     cssEase: "ease",
     arrows: true,
   });
