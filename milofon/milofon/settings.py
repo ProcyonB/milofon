03aedfd7f4217df9a4162e4395277be65a9b47bb
ProcyonB <pelagea.nikolaevna@gmail.com>
2020-08-16 19:30:48 +0300

    secret .env + gitignore

--- milofon/milofon/settings.py
+++ milofon/milofon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q)6wsr9u^hel@l@#nos)_io&on!bkz1hl*u#hr^9!+p0=sqb8p'
+# SECRET_KEY = 'q)6wsr9u^hel@l@#nos)_io&on!bkz1hl*u#hr^9!+p0=sqb8p'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,7 +120,8 @@ USE_TZ = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'milofon.work@gmail.com'
-EMAIL_HOST_PASSWORD = "fmchrzktcgskifpq"
+# EMAIL_HOST_PASSWORD = "fmchrzktcgskifpq"
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'milofon.work@gmail.com'
